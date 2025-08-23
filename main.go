dce5affec066b83ac0f5edf13b03f06587b37884
Cody McCarty <codyjmccarty@gmail.com>
2025-08-22 21:29:00 -0500

    need to refactor some

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -17,13 +19,6 @@ func cleanInput(text string) []string {
 	return result
 }
 
-/** callback for the exit command */
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
 func commandHelp() error {
 	// todo: You can dynamically generate the "usage" section by iterating over my registry of commands. That way the help command will always be up-to-date with the available commands.
 	fmt.Print(`
@@ -36,10 +31,57 @@ exit: Exit the Pokedex
 	return nil
 }
 
+type LocResult struct {
+	Name string
+	URL  string
+}
+type LocArea struct {
+	Count      int
+	Next       *string     // can be null
+	Previous   *string     // can be null
+	LocResults []LocResult `json:"results"`
+}
+
+/** displays the names of 20 location areas in the Pokemon world */
+func commandMap() error {
+	res, err := http.Get("https://pokeapi.co/api/v2/location-area/")
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	var locArea LocArea
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&locArea)
+	if err != nil {
+		return err
+	}
+
+	for _, r := range locArea.LocResults {
+		fmt.Println(r.Name)
+	}
+	return nil
+}
+
+/** It's similar to the map command, however, instead of displaying the next 20 locations, it displays the previous 20 locations. It's a way to go back. */
+func commandMapBack() error {
+	// todo: implement this using config struct of next and prev to hold the next and previous url
+
+	// If you're on the first "page" of results, this command should just print "you're on the first page"
+	return nil
+}
+
+/** contain the Next and Previous URLs that you'll need to paginate */
+type config struct {
+	next *string
+	prev *string
+}
+
 type cliCommand struct {
 	name        string
 	description string
 	callback    func() error
+	config      *config
 }
 
 func main() {
@@ -48,11 +90,25 @@ func main() {
 			name:        "exit",
 			description: "Exits the Pokedex",
 			callback:    commandExit,
+			config:      &config{},
 		},
 		"help": {
 			name:        "help",
 			description: "Displays a help message",
 			callback:    commandHelp,
+			config:      &config{},
+		},
+		"map": {
+			name:        "map",
+			description: "displays the names of 20 location areas in the Pokemon world",
+			callback:    commandMap,
+			config:      &config{},
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "It's similar to the map command, however, instead of displaying the next 20 locations, it displays the previous 20 locations. It's a way to go back.",
+			callback:    commandMapBack,
+			config:      &config{},
 		},
 	}
 
