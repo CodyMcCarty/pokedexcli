0e90246c6b95fcdc5e5f47e1366a9144e92c5677
Cody McCarty <codyjmccarty@gmail.com>
2025-08-26 23:13:46 -0500

    my impl of cache

---
+++ internal/pokecache/cache_test.go
@@ -0,0 +1,68 @@
+package pokecache
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+// Write at least 1 test for your cache package! The tip below should help you get started.
+
+// Test your application manually to make sure that the cache works as expected.
+// When you use the map command to get data for the first time there should be a noticeable waiting time.
+// However, when you use mapb it should be instantaneous because the data for that page is already in the cache.
+// Feel free to add some logging that informs you in the command line when the cache is being used.
+
+func TestAddGet(t *testing.T) {
+	const interval = 5 * time.Second
+	cases := []struct {
+		key string
+		val []byte
+	}{
+		{
+			key: "https://example.com",
+			val: []byte("testdata"),
+		},
+		{
+			key: "https://example.com/path",
+			val: []byte("moretestdata"),
+		},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("Test case %v", i), func(t *testing.T) {
+			cache := NewCache(interval)
+			cache.Add(c.key, c.val)
+			val, ok := cache.Get(c.key)
+			if !ok {
+				t.Errorf("expected to find key")
+				return
+			}
+			if string(val) != string(c.val) {
+				t.Errorf("expected to find value")
+				return
+			}
+		})
+	}
+}
+
+func TestReapLoop(t *testing.T) {
+	const baseTime = 5 * time.Millisecond
+	const waitTime = baseTime + 5*time.Millisecond
+	cache := NewCache(baseTime)
+	cache.Add("https://example.com", []byte("testdata"))
+
+	_, ok := cache.Get("https://example.com")
+	if !ok {
+		t.Errorf("expected to find key")
+		return
+	}
+
+	time.Sleep(waitTime)
+
+	_, ok = cache.Get("https://example.com")
+	if ok {
+		t.Errorf("expected to not find key")
+		return
+	}
+}
